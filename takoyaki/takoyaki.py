cc41a07bea8dfad3f7929c5a4167fbdcf3e96e46
displaynameishere <displaynamecodes@gmail.com>
2025-06-21 00:27:49 +0000

    textual setup

--- takoyaki/takoyaki.py
+++ takoyaki/takoyaki.py
@@ -1,1 +1,17 @@
-print("takoui is bad, gonna use an already made lib for tui")
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Header, Label
+
+class Takoyaki(App):
+    CSS_PATH = "takoyaki.tcss"
+    TITLE = "takoyaki"
+
+    def on_mount(self) -> None:
+        self.screen.styles.background = "black"
+
+    def compose(self) -> ComposeResult:
+        yield Header(id="header")
+
+
+if __name__ == "__main__":
+    app = Takoyaki()
+    app.run()
